23d03521916d40374c38952b2ce4f510bf1c070d
jaluriski17 <110984811+jaluriski17@users.noreply.github.com>
2024-09-24 06:05:20 +0700

    fix map

--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -1,18 +1,16 @@
 import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
 import { IonicModule } from '@ionic/angular';
+import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { HomePage } from './home.page';
-
-import { HomePageRoutingModule } from './home-routing.module';
-
+import { RouterModule } from '@angular/router';
 
 @NgModule({
   imports: [
+    IonicModule,
     CommonModule,
     FormsModule,
-    IonicModule,
-    HomePageRoutingModule
+    RouterModule.forChild([{ path: '', component: HomePage }])
   ],
   declarations: [HomePage]
 })
